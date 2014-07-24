6ce2d63aad4b9c0f0c9ee4f3517184151d0afda3
Noel Delgado <auhcsei@gmail.com>
2014-07-24 00:54:21 -0500

    fuzzy-search merged into master

--- README.md
+++ README.md
@@ -6,17 +6,28 @@ Search and print out lyrics from http://metal-archives.com using the command-lin
 
 ## Usage
 
-1. Clone the repo
-2. Write on the command-line:
-
 ```
-python ~/<path-to-this-repo>/metallum.py <band_name> <song_title>
+python ~/<path-to-repo>/metallum.py <band_name> <song_title>
 ```
 
 Additionally, you can create an alias on your `.zshrc` or `.bash_profile`:
 
 ```
 alias metallum="python ~/<path-to-repo>/metallum.py"
+" Or even shorter
+alias met="python ~/<path-to-repo>/metallum.py"
 ```
 
-Now you can use it like this: `metallum therion quetzalcoatl`
+So you can use it like: `met therion quetzalcoatl`
+
+### You don't have to type the whole thing
+
+Sometimes song titles (or even band names) are too long to type or even to remember them.
+
+Let's put as an example the song ***"In an Excruciating Way Infested with Vermin and Violated by Executioners Who Practise Incendiarism and Desanctifying the Pious"*** by ***"Wormphlegm"***... well, you can get the lyrics by just typing:
+
+`~ met wormphlegm infested`
+
+Or the song ***"The Dark Liege Of Chaos Is Unleashed At The Ensorcelled Shrine Of A'Zura Kai (The Splendour Of A Thousand Swords Gleaming Beneath The Blazon Of The Hyperborean Empire Part II)"*** by ***"Bal-Sagoth"***
+
+`~ met balsagoth chaos`
--- metallum.py
+++ metallum.py
@@ -1,32 +1,28 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 import sys
 import argparse
 import re
 import json
 from urllib import urlopen, urlencode
 
-_site_url = 'http://www.metal-archives.com/'
+site_url = 'http://www.metal-archives.com/'
+lyric_id_re = re.compile(r'id=.+[a-z]+.(?P<id>\d+)')
+band_name_re = re.compile(r'title="(?P<name>.*)\"')
+tags_re = re.compile(r'<[^>]+>')
 
-def _get_song_data(band, song):
+def get_song_data(band, song):
     """Search on metal-archives for song coincidences"""
-    song_params = dict(
+    params = dict(
         bandName = band,
-        exactBandMatch = 1,
-        songTitle = song,
-        exactSongMatch = 1
+        songTitle = song
     )
-    song_params = urlencode(song_params)
-    song_url = _site_url + "search/ajax-advanced/searching/songs?" + song_params
-    return json.load(urlopen(song_url))['aaData']
+    url = "".join([site_url, "search/ajax-advanced/searching/songs?", urlencode(params)])
+    return json.load(urlopen(url))['aaData']
 
-def _get_lyrics_by_song_id(song_id):
+def get_lyrics_by_song_id(song_id):
     """Search on metal-archives for lyrics based on song_id"""
-    tags_re = re.compile(r'<[^>]+>')
-    lyrics_url = _site_url + "release/ajax-view-lyrics/id/%s" % (song_id)
-    lyrics_response = urlopen(lyrics_url)
-    lyrics = lyrics_response.read().strip()
-    lyrics = tags_re.sub('', lyrics) + "\n"
-    return lyrics
+    url = "".join([site_url, "release/ajax-view-lyrics/id/", song_id])
+    return tags_re.sub('', urlopen(url).read().strip())
 
 def main():
     """Runs the program and handles command line options"""
@@ -35,12 +31,19 @@ def main():
     parser.add_argument('song', type=str, help='The title of the song. Ex: "Mopin Carol"')
     args = parser.parse_args()
 
-    song_data = _get_song_data(args.band, args.song)
-    if len(song_data) :
-        id_re = re.compile(r'id=.+[a-z]+.(\d+)')
-        song_id = id_re.search(song_data[0][4]).group(1)
-        print _get_lyrics_by_song_id(song_id)
-    else :
+    song_data = get_song_data(args.band, args.song)
+
+    if len(song_data):
+        data = song_data[0] # use the first coincidence
+        song_id = lyric_id_re.search(data[4]).group("id")
+        band_name = band_name_re.search(data[0]).group("name")
+        song_title = data[3]
+
+        title = "".join([band_name, " - ", data[3], "\n"]).decode('utf-8')
+        lyrics = get_lyrics_by_song_id(song_id).decode('utf-8')
+
+        print "".join([title, "\n", lyrics])
+    else:
         return sys.exit("Lyrics not found")
 
 if __name__ == '__main__':
